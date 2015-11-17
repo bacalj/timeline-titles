e877fac4bc26e469cea9ff1311a7a38d77d33545
Joseph E. Bacal <jebacal@smith.edu>
2015-11-17 12:47:20 -0500

    refactors a little

--- special-timeline.js
+++ special-timeline.js
@@ -8,28 +8,20 @@ timeline = new TL.Timeline(
 	additionalOptions
 );
 
+
 //hide timeline area on load
 $(document).ready(function(){
 	$('.tl-timenav').css('display', 'none');
 	$('.tl-menubar').css('display', 'none');
 });
 
-//hide timeline area when we go back to start
-timeline.on('back_to_start', function(data) {
-   $('.tl-timenav').slideUp();
-   $('.tl-menubar').slideUp();
-});
-
-//show timeline on any change event
-timeline.on('change', function(data) {
-   	$('.tl-timenav').slideDown();
-   	$('.tl-menubar').slideDown();
-});
-
 timeline.on('change', function(data){
-	if (data.unique_id == 'google-spreadsheet-example') {
+	if (data.unique_id == 'google-spreadsheet-example' || data.unique_id == 'another-flickr-example') {
 		$('.tl-timenav').slideUp();
    		$('.tl-menubar').slideUp();
+	} else {
+		$('.tl-timenav').slideDown();
+   		$('.tl-menubar').slideDown();
 	}
 });
 
